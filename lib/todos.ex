95d59c91200cc34a9a9c6a52ce70c0f6b2e439ba
simranjeet <bajwasimran567@gmail.com>
2022-07-25 16:09:51 +0530

    adding create_todos, add_task, contains?, and search_word? function

--- lib/todos.ex
+++ lib/todos.ex
@@ -1,7 +1,23 @@
 defmodule Todos do
-    def create_todos do
+  def add_task(task_list \\ [],number_of_tasks \\ 1) do
+   list= for i <- 1..number_of_tasks do
+      task = IO.gets("ENTER TASK #{i} : ")
+      String.trim(task)
+    end
+    task_list ++ list
+  end
+    def create_todos() do
       number_of_tasks = IO.gets("ENTER THE NO OF TASKS : ")
       {number_of_tasks, _q} = Integer.parse(number_of_tasks)
-      number_of_tasks
+      add_task([],number_of_tasks)
     end
+    def contains_task?(task_list,task) do
+      Enum.member?(task_list,task)
+    end
+    def search_word?(task_list , word) do
+      for task <-task_list , String.contains?(task,word) do
+        task
+      end
+    end
+
 end
