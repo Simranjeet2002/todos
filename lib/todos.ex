0983319db5eafc937ec0de480329c59ffe1104b0
simranjeet <bajwasimran567@gmail.com>
2022-07-25 11:16:43 +0530

    initial commit

---
+++ lib/todos.ex
@@ -0,0 +1,18 @@
+defmodule Todos do
+  @moduledoc """
+  Documentation for `Todos`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Todos.hello()
+      :world
+
+  """
+  def hello do
+    IO.puts("hellooooooo")
+  end
+end
